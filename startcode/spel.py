082b60217d5cc686df06625c7e0901717022a48d
Badlapje <koen@cornelis.it>
2024-03-25 11:45:37 +0100

    Remove solutions

--- startcode/spel.py
+++
@@ -1,6 +0,0 @@
-class Spel:
-    def __init__(self, titel, genre, platform, prijs):
-        self.titel = titel
-        self.genre = genre
-        self.platform = platform
-        self.prijs = prijs
